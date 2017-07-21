bae58158626b4464462963fdce3225e42870a6ed
Charles Levesque <dreamtooloverture@gmail.com>
2017-07-21 16:02:14 -0400

    feat: Added basic StackNavigator to iOS version
    Github issue #9

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -1,58 +1,54 @@
 // @flow
-// import {Navigation} from 'react-native-navigation';
 import React, {Component} from 'react';  // eslint-disable-line
 import {
   AppRegistry,
-  StyleSheet,
   Text,
   View,
   Platform
 } from 'react-native';
+import {StackNavigator} from 'react-navigation';
 
 class HobbyCartes extends Component {
+  static navigationOptions = {
+    title: 'Fil des cartes'
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.{Platform.OS}.js
-        </Text>
+      <View>
+        <Text>Hello, world!</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+// Navigation is done here
+let navigator;
 
+if (Platform.OS === 'ios') {
+  navigator = StackNavigator({
+    Home: {screen: HobbyCartes}
+  });
+} else {  // Android
+  navigator = StackNavigator();
+}
+
+/**
+ * Class that serves as a wrapper. This is useful for testing because tests will
+ * fail if AppRegistry.registerComponent() is called.
+ *
+ * @class Runner
+ */
 class Runner {
   constructor() {
-    AppRegistry.registerComponent('HobbyCartes', () => HobbyCartes);
+    AppRegistry.registerComponent('HobbyCartes', () => navigator);
   }
 }
 
 let App;
 
-if (global.__TEST__) {
-  App = HobbyCartes;
+if (global.__TEST__) {  // __TEST__ is defined in package.json
+  App = navigator;
 } else {
   App = Runner;
 }
