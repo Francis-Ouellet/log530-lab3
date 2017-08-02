0f10c69af90701ef2621f48c2e8cef545ed739a2
Charles Levesque <dreamtooloverture@gmail.com>
2017-08-02 12:31:05 -0400

    feat: Updated members' reducers
    Modification requested in PR #16

--- App/Redux/Reducers/Members.js
+++ App/Redux/Reducers/Members.js
@@ -8,7 +8,6 @@ import {
 } from '../Actions';
 
 export function members(state: Member[] = [], action: {type: Object, data: Object}): Member[] {
-  console.warn(JSON.stringify(action));
   switch (action.type) {
     case RECEIVED_RESPONSE:
       if (action.data.originAction && action.data.originAction === SEARCH && action.data.members) {
@@ -25,8 +24,6 @@ export function members(state: Member[] = [], action: {type: Object, data: Objec
 }
 
 export function currentMember(state: ?Member = null, action: Object): ?Member {
-  console.warn(JSON.stringify(action));
-
   switch (action.type) {
     case RECEIVED_RESPONSE:
       if (action.data.originAction && action.data.originAction === LOGIN) {
